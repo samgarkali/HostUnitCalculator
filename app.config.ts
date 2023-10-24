b3efeee497092b26eca627cd385bd6936d2a4bec
Samgar Kali <samgar.kali@dynatrace.com>
2023-10-24 09:27:20 -0600

    chore: initial Dynatrace App

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+import type { CliOptions } from 'dt-app';
+
+const config: CliOptions = {
+  environmentUrl: 'https://wvp48792.sprint.apps.dynatracelabs.com/',
+  app: {
+    name: 'test-app',
+    version: '0.0.0',
+    description: 'A starting project with routing, fetching data, and charting',
+    id: 'my.test.app',
+    scopes: [{ name: 'storage:logs:read', comment: 'default template' }, { name: 'storage:buckets:read', comment: 'default template' }]
+  },
+};
+
+module.exports = config;
